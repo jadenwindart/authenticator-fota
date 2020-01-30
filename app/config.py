7a506ac2d8845a6b57e64e53c030e78b428b5ace
Andrew Tjanadi <andrewtjanadi@gmail.com>
2020-01-30 12:59:27 +0700

    Second Commit

--- app/config.py
+++ app/config.py
@@ -1,3 +1,7 @@
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = ""
+    POSTGRES_USER = "jlsrslgzboshqr"
+    POSTGRES_PW = "95f1afd463ab357c001ce5eed50561f751612b63a6b06ad45c7292f728c3708"
+    POSTGRES_DB = "dc2u0ijmqe41u3"
+    POSTGRES_URL = "ec2-54-174-229-152.compute-1.amazonaws.com"
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     SQLALCHEMY_TRACK_MODIFICATION = False
