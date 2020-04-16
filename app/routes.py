04e8759f0580b5d82e434c989dd164ddba953369
Andrew Tjanadi <andrewtjanadi@gmail.com>
2020-04-16 15:54:48 +0700

    Session Route

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from .api.device import register_device,get_credentials,authenticate_device,get_device_list
+from .api.device import register_device,get_credentials,authenticate_device,get_device_list,close_session
 from .api.user import login,register
 
 @app.route('/')
@@ -9,6 +9,7 @@ def index():
 app.add_url_rule('/register','register_device',register_device,methods=['POST'])
 app.add_url_rule('/credentials','credentials',get_credentials,methods=['GET'])
 app.add_url_rule('/authenticate','authenticate',authenticate_device,methods=['POST'])
+app.add_url_rule('/close','close_session',close_session,methods=['POST'])
 app.add_url_rule('/list',"device_list",get_device_list,methods=['GET'])
 app.add_url_rule('/user/login','login', login , methods=['POST'])
 app.add_url_rule('/user/register','register',register,methods=['POST'])
